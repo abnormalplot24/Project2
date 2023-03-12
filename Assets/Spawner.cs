ecd5a2c901453cd05a6730778341072327873f63
DevonSteinA <112129702+abnormalplot24@users.noreply.github.com>
2023-03-11 22:58:47 -0500

    Small tweaks

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -9,8 +9,9 @@ using TMPro;
 using UnityEngine.UI;
 public class Spawner : MonoBehaviour
 {
-    public static Vector2d currentLocation;
+    public static Vector3 currentLocation;
     public GameObject cursor;
+    private GameObject interactiveTree;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +20,10 @@ public class Spawner : MonoBehaviour
         if (StateNameController.spawn_anything)
         {
             print("DOO WE GET HEEREE");
-            GameObject interactiveTree = Instantiate(StateNameController.what_to_spawn);
+            interactiveTree = Instantiate(StateNameController.what_to_spawn);
             print(StateNameController.what_to_spawn);
-            currentLocation = new Vector2d(cursor.transform.position.x, cursor.transform.position.y);
-            interactiveTree.transform.position = new Vector3((float)currentLocation.x, (float)currentLocation.y, 5.0f);
+            currentLocation = new Vector3(cursor.transform.position.x, cursor.transform.position.y, cursor.transform.position.z);
+            interactiveTree.transform.position = new Vector3((float)currentLocation.x, (float)currentLocation.y, (float)currentLocation.z + 10.0f);
             interactiveTree.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
             interactiveTree.transform.rotation = new Quaternion(0.0f, 180.0f, 0.0f, 0.0f);
         }
@@ -31,6 +32,12 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (StateNameController.spawn_anything)
+        {
+            currentLocation = new Vector3(cursor.transform.position.x, cursor.transform.position.y, cursor.transform.position.z);
+            interactiveTree.transform.position = new Vector3((float)currentLocation.x, (float)currentLocation.y, (float)currentLocation.z + 10.0f);
+            interactiveTree.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            interactiveTree.transform.rotation = new Quaternion(0.0f, 180.0f, 0.0f, 0.0f);
+        }
     }
 }
--- Assets/isSquirrel.cs
+++ Assets/isSquirrel.cs
@@ -14,6 +14,7 @@ public class isSquirrel : MonoBehaviour
         {
             Destroy(collision.gameObject);
             Destroy(gameObject);
+            print("KILL KILL KILL");
         }
     }
 }
